9ecd722a4f6b99fac5f1d0c4f33e67af71387efa
Nathan Soussana <nathan.soussana@gmail.com>
2023-06-18 14:03:57 +0200

    Added Description to Events and Location to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_175544) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_120235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_175544) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["sport_id"], name: "index_events_on_sport_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
@@ -68,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_175544) do
     t.date "birth_date"
     t.string "gender"
     t.text "about"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
