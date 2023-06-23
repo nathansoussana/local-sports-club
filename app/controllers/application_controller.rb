3731ac8e74f9547c6c998a5a51bb8a2842cc14c6
Eren Bastaymaz <bastaymaz@gmail.com>
2023-06-23 14:33:44 +0300

    user controller created, sign up still doesn't work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit::Authorization
+
+  # ...
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :birth_date, :about])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :birth_date, :about])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  root to: "pages#home"
+  get '/pages/components', to: 'pages#components'
+  get 'users/:id', to: 'users#show', as: 'user'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pages#home"
 
-  get '/pages/components', to: 'pages#components'
+
+
 
   resources :events
+  resources :users
 end
