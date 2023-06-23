bdb0bbea0e972ee5584b36006625ba4531a31eeb
Eren Bastaymaz <bastaymaz@gmail.com>
2023-06-23 16:38:54 +0300

    sign up page created with users controller, it is bugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,45 @@
 class UsersController < ApplicationController
-end
-
-def show
-  @user = User.find(params[:id])
-end
-
-def new
-  @user = User.new
-end
-
-def create
-  @user = User.new(user_params)
-
-  if @user.save
-    redirect_to user_path(@user), notice: "User was successfully added."
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    @user = User.find(params[:id])
   end
-end
 
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find(params[:id])
-
-  if @user.update(user_params)
-    redirect_to user_path(@user)
-  else
-    render :edit, status: :unprocessable_entity
+  def new
+    @user = User.new
   end
-end
 
-def destroy
-  @user = User.find(params[:id])
-  @user.destroy
-  redirect_to users_path, status: :see_other
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user), notice: "User was successfully added."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, status: :see_other
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :birth_date, :gender, :about_me, :profile_picture)
+  end
 end
