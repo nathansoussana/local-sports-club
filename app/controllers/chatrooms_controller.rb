8fd2422b25ba8d8e267910f6573e881882c906d1
Anton Loof <antonloof@telia.com>
2023-06-30 15:23:10 +0200

    deleted chatroom controller+view, we do not need them

--- app/controllers/chatrooms_controller.rb
+++
@@ -1,9 +0,0 @@
-class ChatroomsController < ApplicationController
-  def show
-    @event = Event.find(params[:event_id])
-    @chatroom = @event.chatroom
-    @messages = @chatroom.messages
-    @message = Message.new
-    authorize @chatroom
-  end
-end
