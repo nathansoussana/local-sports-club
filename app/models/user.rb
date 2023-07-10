20617a1725222c00567f2b955d22a4836c4b537f
Nathan Soussana <nathan.soussana@gmail.com>
2023-07-10 17:03:57 +0200

    Fixed typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :user_sports, dependent: :destroy
   has_many :sports, through: :user_sports
   has_one_attached :photo
-  has_many :massages, dependent: :destroy
+  has_many :messages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
